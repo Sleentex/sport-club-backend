cd257babf55d69e75cbf24218bc18726acd99a4f
Sleentex <56062311+Sleentex@users.noreply.github.com>
2021-10-07 23:51:46 +0300

    Add model and migration for trainer

---
+++ app/Models/Trainer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *
+ * @OA\Schema(
+ * required={"first_name", "last_name", "email", "phone_number", "password"},
+ * @OA\Xml(name="User"),
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="user_id", type="integer", example="1", description="Refernce to user relation"),
+ * @OA\Property(property="created_at", ref="#/components/schemas/BaseModel/properties/created_at"),
+ * @OA\Property(property="updated_at", ref="#/components/schemas/BaseModel/properties/updated_at"),
+ * )
+ *
+ * Class User
+ *
+ */
+
+class Trainer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function trainer()
+    {
+        return $this->belongsTo(Trainer::class);
+    }
 }
